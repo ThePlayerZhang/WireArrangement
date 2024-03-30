6fedaee706682ebd81caabbcbb978ff4c91d0e89
ThePlayerZhang <zhangtangtang2021@126.com>
2024-03-30 22:15:22 +0800

    202403302214

---
+++ config.py
@@ -0,0 +1,4 @@
+timestamp = "%Y-%m-%d %H:%M:%S"
+frame_size = (1280, 720)
+frame_bg_color = (40, 70, 130)
+output = "[%s][%s] %s"
---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+import frame
+import timestamp as t
+pygame.init()
+
+t.output("Hello from WireArrangement, the first output!", t.i)
+
+main_frame = frame.Frame()
+while True:
+    main_frame.mainloop()
