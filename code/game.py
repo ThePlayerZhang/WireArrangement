d38904cbb803f5b96947cd0c42725a7da16f84c2
ThePlayerZhang <zhangtangtang2021@126.com>
2024-04-04 13:09:24 +0800

    202404041309

--- code/game.py
+++ code/game.py
@@ -1,7 +1,7 @@
 import pygame
 from .log import o
 from .frame import Frame
-from config import *
+from res.init import *
 pygame.init()
 o("游戏模块加载成功！")
 
@@ -11,4 +11,8 @@ class Game(Frame):
         super().__init__()
 
     def command(self):
-        pass
+        self.window.blit(img[self.frame], (0, 0))
+        if self.frame == "ui.main":
+            self.window.blit(img["ui.main.button.start"], (0, 0))
+            self.window.blit(img["ui.main.button.settings"], (0, 0))
+            self.window.blit(img["ui.main.button.exit"], (0, 0))
---
+++ res/init.py
@@ -0,0 +1,7 @@
+from pygame.image import load as l
+img = {
+    "ui.main": l("./res/bg/main.jpg"),
+    "ui.main.button.start": l("./res/but/main/start.png"),
+    "ui.main.button.settings": l("./res/but/main/settings.png"),
+    "ui.main.button.exit": l("./res/but/main/exit.png")
+}
