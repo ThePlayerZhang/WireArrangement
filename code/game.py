17d7371a1a5bded719f8c3eb0b94e2ebe625344b
ThePlayerZhang <zhangtangtang2021@126.com>
2024-04-04 14:01:36 +0800

    202404041401

--- code/game.py
+++ code/game.py
@@ -10,6 +10,7 @@ o("游戏模块加载成功！")
 class Game(Frame):
     def __init__(self):
         super().__init__()
+        self.main_ui_y = 0
 
     def click_command(self, pos1, pos2):
         if pos1[0] < self.mouse[0] < pos2[0] and pos1[1] < self.mouse[1] < pos2[1]:
@@ -18,19 +19,19 @@ class Game(Frame):
             return False
 
     def command(self):
-        self.window.blit(img[self.frame], (0, 0))
+        self.window.blit(img[self.frame.replace(".a", "")], (0, 0))
         if self.frame == "ui.main":
             if self.click_command((200, 280), (570, 480)):
                 self.window.blit(img["ui.main.button.start.click"], (0, 0))
                 if pygame.mouse.get_pressed(3)[0]:
-                    self.frame = "ui.start"
+                    self.frame = "ui.start.a"
             else:
                 self.window.blit(img["ui.main.button.start"], (0, 0))
 
             if self.click_command((640, 470), (1000, 600)):
                 self.window.blit(img["ui.main.button.settings.click"], (0, 0))
                 if pygame.mouse.get_pressed(3)[0]:
-                    self.frame = "ui.setting"
+                    self.frame = "ui.setting.a"
             else:
                 self.window.blit(img["ui.main.button.settings"], (0, 0))
 
@@ -40,3 +41,12 @@ class Game(Frame):
                     quit_game()
             else:
                 self.window.blit(img["ui.main.button.exit"], (0, 0))
+
+        if self.frame in ["ui.start.a", "ui.setting.a"]:
+            if self.main_ui_y > 500:
+                self.frame = self.frame.replace(".a", "")
+            else:
+                self.main_ui_y += 10
+                self.window.blit(img["ui.main.button.start"], (0, self.main_ui_y))
+                self.window.blit(img["ui.main.button.settings"], (0, self.main_ui_y))
+                self.window.blit(img["ui.main.button.exit"], (0, self.main_ui_y))
--- res/init.py
+++ res/init.py
@@ -1,13 +1,14 @@
-from pygame.image import load as l
+from pygame.image import load as i
 
 img = {
-    "ui.main": l("./res/bg/main.jpg"),
-    "ui.main.button.start": l("./res/but/main/start.png"),
-    "ui.main.button.settings": l("./res/but/main/settings.png"),
-    "ui.main.button.exit": l("./res/but/main/exit.png"),
-    "ui.main.button.start.click": l("./res/but/main/start_click.png"),
-    "ui.main.button.settings.click": l("./res/but/main/settings_click.png"),
-    "ui.main.button.exit.click": l("./res/but/main/exit_click.png"),
-    "ui.setting": l("./res/bg/main.jpg"),
-    "ui.start": l("./res/bg/main.jpg")
+    "ui.main": i("./res/bg/main.jpg"),
+    "ui.main.button.start": i("./res/but/main/start.png"),
+    "ui.main.button.settings": i("./res/but/main/settings.png"),
+    "ui.main.button.exit": i("./res/but/main/exit.png"),
+    "ui.main.button.start.click": i("./res/but/main/start_click.png"),
+    "ui.main.button.settings.click": i("./res/but/main/settings_click.png"),
+    "ui.main.button.exit.click": i("./res/but/main/exit_click.png"),
+
+    "ui.setting": i("./res/bg/main.jpg"),
+    "ui.start": i("./res/bg/main.jpg")
 }
